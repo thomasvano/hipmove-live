7b9685c1d1515e1112de28e403c24dbe4763d001
Thomas Vanoverschelde <thomas.vanoverschelde@gmail.com>
2020-05-02 19:16:59 +0200

    oo

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const express = require("express")
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server)
